86b89bf4ea9553dd8994a3fd0f783ed4d4d3d4b0
Lalitwadee Damyos <rainbow.prae@gmail.com>
2019-07-24 18:19:57 +0700

    initailize project & add Formik

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import {
+  Formik, Form, Field, ErrorMessage,
+} from 'formik'
+
+const App = () => (
+  <div>
+    <h1>Basic form!</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={(values) => {
+        const errors = {}
+        if (!values.email) {
+          errors.email = 'Required'
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+          errors.email = 'Invalid email address'
+        }
+        return errors
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2))
+          setSubmitting(false)
+        }, 400)
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+
+    <h1>Render Prop!</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={(values) => {
+        const errors = {}
+        if (!values.email) {
+          errors.email = 'Required'
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+          errors.email = 'Invalid email address'
+        }
+        return errors
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2))
+          setSubmitting(false)
+        }, 400)
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+)
+
+export default App
