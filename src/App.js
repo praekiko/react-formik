160d6e5e93ccf8e15fd208f04e9fec364d9a2146
Lalitwadee Damyos <rainbow.prae@gmail.com>
2019-07-26 14:19:21 +0700

    Formik with react select and Yup

--- src/App.js
+++ src/App.js
@@ -1,92 +1,126 @@
 import React from 'react'
-import {
-  Formik, Form, Field, ErrorMessage,
-} from 'formik'
+import Select from 'react-select'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+
+const options = [
+  { value: 'Food', label: 'Food' },
+  { value: 'Being Fabulous', label: 'Being Fabulous' },
+  { value: 'Ken Wheeler', label: 'Ken Wheeler' },
+  { value: 'ReasonML', label: 'ReasonML' },
+  { value: 'Unicorns', label: 'Unicorns' },
+  { value: 'Kittens', label: 'Kittens' },
+]
+
+class MySelect extends React.Component {
+  handleChange = value => {
+    // this is going to call setFieldValue and manually update values.topcis
+    this.props.onChange('topics', value)
+  }
+
+  handleBlur = () => {
+    // this is going to call setFieldTouched and manually update touched.topcis
+    this.props.onBlur('topics', true)
+  }
+
+  render() {
+    return (
+      <div style={{ margin: '1rem 0' }}>
+        <label htmlFor="color">Topics (select at least 3) </label>
+        <Select
+          id="color"
+          options={options}
+          isMulti
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          value={this.props.value}
+        />
+        {!!this.props.error && this.props.touched && (
+          <div style={{ color: 'red', marginTop: '.5rem' }}>{this.props.error}</div>
+        )}
+      </div>
+    )
+  }
+}
 
 const App = () => (
-  <div>
-    <h1>Basic form!</h1>
+  <div className="app">
     <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={(values) => {
-        const errors = {}
-        if (!values.email) {
-          errors.email = 'Required'
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-          errors.email = 'Invalid email address'
-        }
-        return errors
+      initialValues={{
+        email: '',
+        topics: [],
       }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email('Invalid email address')
+          .required('Email is required!'),
+        topics: Yup.array()
+          .min(3, 'Pick at least 3 tags')
+          .of(
+            Yup.object().shape({
+              label: Yup.string().required(),
+              value: Yup.string().required(),
+            }),
+          ),
+      })}
       onSubmit={(values, { setSubmitting }) => {
+        const payload = {
+          ...values,
+          topics: values.topics.map(t => t.value),
+        }
         setTimeout(() => {
-          alert(JSON.stringify(values, null, 2))
+          alert(JSON.stringify(payload, null, 2))
           setSubmitting(false)
-        }, 400)
+        }, 1000)
       }}
     >
       {({
         values,
-        errors,
         touched,
+        dirty,
+        errors,
         handleChange,
         handleBlur,
         handleSubmit,
+        handleReset,
+        setFieldValue,
+        setFieldTouched,
         isSubmitting,
-        /* and other goodies */
       }) => (
         <form onSubmit={handleSubmit}>
+          <label htmlFor="email" style={{ display: 'block' }}>
+            Email
+          </label>
           <input
+            id="email"
+            placeholder="Enter your email"
             type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
             value={values.email}
-          />
-          {errors.email && touched.email && errors.email}
-          <input
-            type="password"
-            name="password"
             onChange={handleChange}
             onBlur={handleBlur}
-            value={values.password}
           />
-          {errors.password && touched.password && errors.password}
+          {errors.email && touched.email && (
+            <div style={{ color: 'red', marginTop: '.5rem' }}>{errors.email}</div>
+          )}
+          <MySelect
+            value={values.topics}
+            onChange={setFieldValue}
+            onBlur={setFieldTouched}
+            error={errors.topics}
+            touched={touched.topics}
+          />
+          <button
+            type="button"
+            className="outline"
+            onClick={handleReset}
+            disabled={!dirty || isSubmitting}
+          >
+            Reset
+          </button>
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
         </form>
-      )}
-    </Formik>
-
-    <h1>Render Prop!</h1>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={(values) => {
-        const errors = {}
-        if (!values.email) {
-          errors.email = 'Required'
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-          errors.email = 'Invalid email address'
-        }
-        return errors
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2))
-          setSubmitting(false)
-        }, 400)
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
       )}
     </Formik>
   </div>
